37aba0aa1d463003b88ca1c94ab8e38454e9f168
xyztoshi <dev.magmacraft@gmail.com>
2026-09-20 17:00:03 +0200

    fix

--- src/components/explore/ExploreThemes.tsx
+++ src/components/explore/ExploreThemes.tsx
@@ -44,7 +44,7 @@ export default function ExploreThemes() {
   const [search, setSearch] = createSignal("");
 
   const REMOTE_THEMES_URL =
-    "https://raw.githubusercontent.com/nexcord-app/themes/master/themes.json";
+    "https://raw.githubusercontent.com/nexcord-app/themes/main/themes.json";
 
   onMount(() => {
     header.updateHeader({
